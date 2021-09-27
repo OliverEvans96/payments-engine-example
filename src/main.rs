a560fc4d96b53c8e5a37044940727f38ad8590ff
Oliver Evans <oliverevans96@gmail.com>
2021-09-27 08:55:41 -0700

    Parse command line args with structopt / clap

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,40 @@
-use std::env;
 use std::error::Error;
 use std::fs;
 use std::io;
+use std::path;
+
+use structopt::StructOpt;
 
 use payments_engine_example::process_transactions;
 
-// TODO: CL args
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "payments-engine-example",
+    version = "0.1",
+    author = "Oliver Evans <oliverevans96@gmail.com>",
+    about = "Simple engine to process streaming financial transactions and write final account balances as output"
+)]
+struct CliOpts {
+    /// Path to transactions CSV file
+    #[structopt(parse(from_os_str))]
+    input_csv_path: path::PathBuf,
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     // Allow log level to be set via env vars without recompiling
     env_logger::init();
 
-    // First arg is path to executable, not important
-    let mut args = env::args().skip(1);
-
-    // TODO: Clean up CLI, add help, etc.
-    let input_csv_path = args
-        .next()
-        .expect("Missing required command line argument - input csv path");
-
-    // Check for extraneous arguments
-    if args.len() > 0 {
-        log::warn!(
-            "unused command line arguments: {:?}",
-            args.collect::<Vec<_>>()
-        );
-    }
+    let opts = CliOpts::from_args();
 
     // Open file and process transactions, writing to stdout
-    if let Ok(mut input_file) = fs::File::open(&input_csv_path) {
+    if let Ok(mut input_file) = fs::File::open(&opts.input_csv_path) {
         // TODO: Handle errors
         process_transactions(&mut input_file, &mut io::stdout())?;
     } else {
-        log::error!("Could not open input file '{}'", input_csv_path);
+        log::error!(
+            "Could not open input file '{}'",
+            &opts.input_csv_path.to_str().unwrap_or("<invalid path>")
+        );
     }
 
     // :)
