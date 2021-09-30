508d25718518334a78646ec79de0aca26d8e0b60
Oliver Evans <oliverevans96@gmail.com>
2021-09-29 18:15:28 -0700

    Fix currency rounding - apparently '^' is bitwise XOR, not exponentiation

--- src/currency.rs
+++ src/currency.rs
@@ -4,13 +4,27 @@ pub type CurrencyFloat = f32;
 pub fn round_currency(amount: CurrencyFloat) -> CurrencyFloat {
     const NUM_DIGITS: u8 = 4;
     // Round to NUM_DIGITS decimal places
-    let multiplier: CurrencyFloat = (10 ^ NUM_DIGITS).into();
+    let multiplier: CurrencyFloat = 10.0f32.powf(NUM_DIGITS.into());
     (amount * multiplier).round() / multiplier
 }
 
 pub fn floor_currency(amount: CurrencyFloat) -> CurrencyFloat {
     const NUM_DIGITS: u8 = 4;
-    // Round to NUM_DIGITS decimal places
-    let multiplier: CurrencyFloat = (10 ^ NUM_DIGITS).into();
+    // Round down to NUM_DIGITS decimal places
+    let multiplier: CurrencyFloat = 10.0f32.powf(NUM_DIGITS.into());
     (amount * multiplier).floor() / multiplier
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_round_currency() {
+        use super::round_currency;
+
+        assert_eq!(round_currency(1.00003), 1.0);
+        assert_eq!(round_currency(0.0001), 0.0001);
+        assert_eq!(round_currency(0.002), 0.002);
+        assert_eq!(round_currency(0.00005), 0.0001);
+        assert_eq!(round_currency(0.00004), 0.0);
+    }
 }
